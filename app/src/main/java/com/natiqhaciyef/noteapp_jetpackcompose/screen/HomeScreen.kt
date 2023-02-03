3c95297bff481300de0daa06bc2ecc0fd8b3e544
Natiq <natiq00h2272@gmail.com>
2023-02-03 21:16:36 +0400

    Home Screen Design started

--- app/src/main/java/com/natiqhaciyef/noteapp_jetpackcompose/screen/HomeScreen.kt
+++ app/src/main/java/com/natiqhaciyef/noteapp_jetpackcompose/screen/HomeScreen.kt
@@ -1,10 +1,46 @@
 package com.natiqhaciyef.noteapp_jetpackcompose.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.natiqhaciyef.noteapp_jetpackcompose.ui.theme.*
 
 @Preview
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            Box(
+                modifier = Modifier
+                    .size(55.dp)
+                    .clip(shape = CircleShape)
+                    .background(Aquatic),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Note Buttonn",
+                    modifier = Modifier.size(32.dp),
+                    tint = Color.Black
+                )
+            }
+        }
+    ) {
 
+    }
 }
+

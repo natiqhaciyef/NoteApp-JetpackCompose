6cfbcd008551a6acd699aa37f07393dfaca56533
Natiq <natiq00h2272@gmail.com>
2023-02-04 20:34:08 +0400

    Home Screen Recycler view ready for release

---
+++ app/src/main/java/com/natiqhaciyef/noteapp_jetpackcompose/model/NoteModel.kt
@@ -0,0 +1,8 @@
+package com.natiqhaciyef.noteapp_jetpackcompose.model
+
+data class NoteModel(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val date: String
+)
--- app/src/main/java/com/natiqhaciyef/noteapp_jetpackcompose/screen/HomeScreen.kt
+++ app/src/main/java/com/natiqhaciyef/noteapp_jetpackcompose/screen/HomeScreen.kt
@@ -1,11 +1,12 @@
 package com.natiqhaciyef.noteapp_jetpackcompose.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -14,13 +15,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.natiqhaciyef.noteapp_jetpackcompose.model.NoteModel
 import com.natiqhaciyef.noteapp_jetpackcompose.ui.theme.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Preview
 @Composable
-fun HomeScreen() {
+fun HomeScreen(noteList: MutableList<NoteModel> = mutableListOf()) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         floatingActionButton = {
@@ -40,7 +47,73 @@ fun HomeScreen() {
             }
         }
     ) {
+        LazyColumn {
+            items(items = noteList) { selectedItem ->
+                LazyColumnRows(selectedItem) {
 
+                }
+            }
+        }
     }
 }
 
+@Preview
+@Composable
+fun LazyColumnRows(note: NoteModel =
+                       NoteModel(id= 0 , "Text","desc intro bust","21-01-2023"),
+                   content: () -> Unit = { }) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(90.dp)
+            .padding(horizontal = 10.dp, vertical = 7.dp)
+            .clickable {
+                content()
+            },
+        shape = RoundedCornerShape(12.dp),
+        elevation = 5.dp,
+        backgroundColor = LightBrown,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = note.title,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 3.dp)
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Text(
+                text = note.description,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 3.dp)
+            )
+        }
+    }
+}
+
+
+// Will add in view-model
+fun calendarFormatter(calendar: Calendar): String {
+    val format = "dd-MM-yyyy"
+    val sdf = SimpleDateFormat(format, Locale.UK)
+    return sdf.format(calendar.time)
+}
+
+
+// Will add in view-model
+fun calendarFormatterForWeekly(calendar: Calendar): String {
+    return calendar.get(Calendar.DAY_OF_WEEK).toString()
+}
